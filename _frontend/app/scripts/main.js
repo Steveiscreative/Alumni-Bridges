27ec62242d42c165e9a36628225f19660545ed94
Steve Stephenson <ttwstephenson@gmail.com>
2012-11-23 17:07:25 -0500

    Frontend UI development, sidebar, JS, main panel

--- _frontend/app/scripts/main.js
+++ _frontend/app/scripts/main.js
@@ -10,6 +10,25 @@ require.config({
 });
  
 require(['app'], function(app) {
-//
+
+$('.app-panel-view-tog').on('click', function(e) {
+	e.preventDefault();
+	$('#sidebar').toggleClass('app-mini-sidebar');
+	$('#main').toggleClass('app-full-view');
+	$('.full-width-nav-item').toggleClass('hide');
+	$('.mini-nav-item').toggleClass('hide');
+	$(this).find('i').toggleClass('icon-chevron-right','icon-chevron-right');
+
+});
+
+$('#advancedSearch').on('click', function(e){
+	e.preventDefault(); 
+	$('.app-advanced-search').toggleClass('hide');
+});
+
+$('.close').on('click', function(){
+	$(this).parent().addClass('hide');
+});
+
   console.log(app);
 });
